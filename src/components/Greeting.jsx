8524be1dc8e91ccb973139d2a35adfc07a8281ad
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-07-31 21:58:50 +0500

    initail commit

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+<>
+<div>
+    <div className='text-gray-300'>
+    Hi Mr.Michael
+    </div>
+    <div className='font-bold'>
+        Welcome Back!
+    </div>
+</div>
+</>
+  )
+}
+
+export default Greeting
